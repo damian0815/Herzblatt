146a41bfb1638eefaaeb8957bb9c39d34d1e3472
Damian at mba <damian@frey.NOSPAMco.nz>
2018-12-14 20:58:24 +0100

    working title screen

--- herzblatt/game/game.js
+++ herzblatt/game/game.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
+    width: 800,
+    height: 640,
     backgroundColor: '#000000',
     scene: [ titleScene, characterSelectScene ]
 };
--- herzblatt/game/titleScene.js
+++ herzblatt/game/titleScene.js
@@ -11,20 +11,29 @@ var titleScene = new Phaser.Class({
 
     preload: function ()
     {
-        this.load.image('lensThing', 'assets/pics/lensthing.JPG');
+        this.load.image('title', 'assets/pics/title.jpg');
+        this.load.image('play', 'assets/buttons/play.png');
     },
 
     create: function ()
     {
-        this.add.sprite(400, 300, 'lensThing').setAlpha(0.8);
+        this.add.sprite(400, 200, 'title');
 
-        this.input.once('pointerdown', function () {
+        this.add.text(100, 100, 'Herzblatt');
+        this.add.text(100, 400, 'A game by Some People for Klagenfurt Winter Game Jam 2018');
 
-            console.log('From titleScene to characterSelectScene');
-
-            this.scene.start('characterSelectScene');
-
-        }, this);
+        var playButton = this.add.sprite(400, 500, 'play').setInteractive();
+        var that = this;
+        playButton.on('pointerover', function(pointer) {
+            this.setTint(0xffaaaa);
+        });
+        playButton.on('pointerout', function(pointer) {
+            this.clearTint();
+        });
+         playButton.on('pointerdown', function(pointer) {
+            console.log('Clicked play, going to characterSelectScene');
+            that.scene.start('characterSelectScene');
+        });
     }
 
 });
