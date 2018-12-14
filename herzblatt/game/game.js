6c5a342aac5ad50a539f1f0ca6a9f45fe5b67c29
Damian at mba <damian@frey.NOSPAMco.nz>
2018-12-14 19:56:24 +0100

    initial commit

---
+++ herzblatt/game/game.js
@@ -0,0 +1,32 @@
+define(["require", "exports", './objects/Player'], function(require, exports, __Game__) {
+    var Game = __Game__;
+
+    (function () {
+        var myGame = new Phaser.Game(this, 'game', 400, 400, init, create, update, render);
+        var myPlayer;
+        var shake;
+        var x = 1;
+        function init() {
+            myGame.stage.scaleMode = Phaser.StageScaleMode.NO_SCALE;
+            myGame.loader.addImageFile('jet', 'assets/jets.png');
+            myGame.loader.load();
+        }
+        function create() {
+            myPlayer = new Game.Player(myGame, 100, 100, 'jet');
+            shake = myGame.camera.fx.add(Phaser.FX.Camera.Shake);
+        }
+        function update() {
+            if(myGame.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+                shake.start(0.07, 1);
+            }
+        }
+        function render() {
+            var g = (Math.sin(x) * 128 + 127) | 0;
+            x += Math.PI / 32;
+            myGame.stage.context.fillStyle = 'rgb(255, ' + g + ', 255)';
+            myGame.stage.context.font = 'bold 18px Arial';
+            myGame.stage.context.fillText(Phaser.VERSION + " works!", myGame.stage.centerX - 120, myGame.stage.centerY);
+        }
+    })();
+})
+//@ sourceMappingURL=game.js.map
---
+++ herzblatt/readme.md
@@ -0,0 +1,20 @@
+# Phaser WebStorm Template
+
+This project is a simple set of starting files to get a Phaser game started using the WebStorm IDE for TypeScript editing, and Chrome DevTools for live debugging of TypeScript via sourcemaps.
+
+See the tutorial on [my website](http://ericterpstra.com/2013/06/phaser-starter-project-for-webstorm-and-chrome-modular-typescript-code-completion-and-live-debugging/) to get started.
+
+
+### Notable Features
+
+* TypeScript code-hinting, intellisense, error checking
+* File watchers to generate .js and sourcemaps on save
+* Require.js used for modular typescript (one class and/or module per file)
+* Live debugging of modular TypeScript with Chrome DevTools (including breakpoints, watchers, etc...)
+
+*For Best Results, Please use Webstorm 6 EAP ( >= build 129.541)*
+
+Thanks to:
+* [Photon Storm](http://www.photonstorm.com)
+* [Jesse Freeman](http://www.jessefreeman.com)
+* [Luke Hoban](https://twitter.com/lukehoban)
