e61cf9780f6a2b4948f220a76b8aed0d70c4ea86
Martin Scheiber <mascheiber@edu.aau.at>
2018-12-17 13:03:57 +0100

    Submittet AAUßKLU 2nd WinterGameJam Version

--- herzblatt/game/scenes/resetScene.js
+++ herzblatt/game/scenes/resetScene.js
@@ -12,6 +12,7 @@ var resetScene = new Phaser.Class({
     preload: function ()
     {
         this.load.image('bg', 'assets/pics/bg.jpg');
+
     },
 
     create: function ()
@@ -28,6 +29,51 @@ var resetScene = new Phaser.Class({
         var revealDecScene = this.scene.get('revealDecisionScene');
         var wonScene = this.scene.get('youWonScene');
         var lostScene = this.scene.get('youLostScene');
+
+        this.add.text(100,20, "Programming\n" +
+            "Martin Scheiber, Damian Stewart\n" +
+            "\n" +
+            "Writing\n" +
+            "Marion Berg\n" +
+            "\n" +
+            "Graphics Artist\n" +
+            "Stefanie Marko, Georg Wieser, Kristina Wogatai\n" +
+            "\n" +
+            "Recording Engineer\n" +
+            "Philipp Überbacher\n" +
+            "\n" +
+            "Music\n" +
+            "Damian Stewart\n" +
+            "\n" +
+            "Voice #1\n" +
+            "Sarah Wachter\n" +
+            "\n" +
+            "\n" +
+            "Voice #2\n" +
+            "John NA Brown\n" +
+            "\n" +
+            "\n" +
+            "Voice #3\n" +
+            "Marion Berg\n" +
+            "\n" +
+            "\n" +
+            "Voice #4\n" +
+            "Mathew Sherry\n" +
+            "\n" +
+            "Announcer\n" +
+            "Felix Schniz\n" +
+            "\n" +
+            "Player Gibberish\n" +
+            "Philipp Überbacher\n" +
+            "\n" +
+            "Whistler\n" +
+            "Simon Bernard, John NA Brown\n" +
+            "\n" +
+            "Bachelor voice\n" +
+            "eSpeak\n" +
+            "\n" +
+            "Cheers\n" +
+            "The Awesome Game Jam People").setOrigin(0,0);
 
         // titleScene.scene.remove();
         // loadingScene.scene.remove();
